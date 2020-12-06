26e329b315fde07d78a91489b639d668c33e0265
Richard Szczerba <richardszczerba@gmail.com>
2020-12-06 13:30:32 +0100

    change epch size

--- train.py
+++ train.py
@@ -255,7 +255,7 @@ def train_cephalo(name, landmarks, load=False, startEpoch=0, batched=False, fold
     print(f"AVG? {avg_labels}, RMS? {rms}")
     print(f"BEGIN {name} {landmarks}")
     batchsize=2
-    num_epochs=2
+    num_epochs=40
     device = 'cuda'
 
 
